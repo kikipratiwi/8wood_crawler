c56135cec188e174b67a21f6401cdaf59972f90f
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-01-29 13:23:04 +0700

    enable scrapy image pipelines

--- EightWoodCrawler/EightWoodCrawler/settings.py
+++ EightWoodCrawler/EightWoodCrawler/settings.py
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'EightWoodCrawler.pipelines.EightwoodcrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'scrapy.pipelines.images.ImagesPipeline': 1
+}
+IMAGES_STORE = 'tmp/images/'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
