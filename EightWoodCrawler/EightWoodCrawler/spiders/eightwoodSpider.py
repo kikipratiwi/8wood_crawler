dec425840a3d01c5374f730d0d78cb4942409eea
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-01-29 13:24:33 +0700

    set image_url for scrapy image pipeline

--- EightWoodCrawler/EightWoodCrawler/spiders/eightwoodSpider.py
+++ EightWoodCrawler/EightWoodCrawler/spiders/eightwoodSpider.py
@@ -15,7 +15,7 @@ class EightwoodspiderSpider(scrapy.Spider):
             with open(os.path.join(os.path.dirname(__file__), "../resources/categories.csv")) as categories:
                 for category in csv.DictReader(categories):
                     category_text=category["category"]
-                    url="http://www.8wood.id/pakaian/".format(category_text)
+                    url="http://www.8wood.id/pakaian/"+category_text+"/"
                     # The meta is used to send our search text into the parser as metadata
                     yield scrapy.Request(url, callback = self.parse, meta = {"category_text": category_text})
 
@@ -34,13 +34,10 @@ class EightwoodspiderSpider(scrapy.Spider):
             XPATH_PRODUCT_IMAGE_LINK=".//div[@class='img']//a[@class='product-image-custom']//img[contains(@class,'imgThumProduct')]/@data-original"
             XPATH_PRODUCT_LINK=".//div[@class='desc']//h5/a/@href"
 
-            XPATH_PAGINATION_LINK=".//ul[@class='pagination']//li[@class='next']//a/@data-page"
-
             raw_product_name=product.xpath(XPATH_PRODUCT_NAME).extract()
             raw_product_price=product.xpath(XPATH_PRODUCT_PRICE).extract()
             raw_product_image_link=product.xpath(XPATH_PRODUCT_IMAGE_LINK).extract()
             raw_product_link=product.xpath(XPATH_PRODUCT_LINK).extract()
-            raw_pagination_link=product.xpath(XPATH_PAGINATION_LINK).extract()
             
             # cleaning the data
             product_name=''.join(raw_product_name).strip(
@@ -49,15 +46,15 @@ class EightwoodspiderSpider(scrapy.Spider):
             ) if raw_product_price else None
             product_image_link=''.join(raw_product_image_link).strip(
             ) if raw_product_image_link else None
-            product_link=EightwoodspiderSpider.start_urls[0].join(raw_product_link).strip(
+            product_link=str(EightwoodspiderSpider.start_urls).join(raw_product_link).strip(
             ) if raw_product_link else None
-            pagination_link=''.join(raw_pagination_link).strip(
-            ) if raw_pagination_link else None
 
             yield {
                 'product_name': product_name,
                 'product_price': product_price,
-                'product_image_link': product_image_link,
                 'product_link': product_link,
+                'image_urls' : raw_product_image_link,
+                'images': product_name,
                 'category': product_category
             }
+
